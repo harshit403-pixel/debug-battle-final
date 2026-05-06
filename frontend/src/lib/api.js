13bdee8f15462b6264fc097fc586dd7801bf3bd7
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-05-06 22:21:33 +0530

    Update baseURL in api.js to remove unnecessary path segment

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://kodexdebugbattle4.onrender.com/api',
+  baseURL: 'https://kodexdebugbattle4.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 });
 
