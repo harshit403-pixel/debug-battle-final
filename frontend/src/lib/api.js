fd8d5709540164c7091e8aec7237cf17f4ae23b5
Harshit Raghuwanshi <harshuraghu7999@gmail.com>
2026-05-06 22:26:19 +0530

    asd

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://kodexdebugbattle4.onrender.com/api',
+  baseURL: 'https://kodexdebugbattle4.onrender.com/ap',
   headers: { 'Content-Type': 'application/json' },
 });
 
